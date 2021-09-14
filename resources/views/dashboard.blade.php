9e521c874159b5cb5c0527feb7dca28caed18dd8
Katalam <info@katalam.com>
2021-09-14 18:08:32 +0200

    Added views for dashboard and show of user

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,19 +1,23 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="w-full rounded-md">
-                    <form action="{{ route('show') }}" method="POST">
-                        @csrf
-                        <input class="w-full rounded-md border-0" name="username" type="text">
-                        <button type="submit">Submit</button>
-                    </form>
+                <div class="w-full rounded-md grid grid-cols-6">
+                    <div class="col-span-4 col-start-2 px-4 py-4">
+                        <form action="{{ route('uprofile.search') }}" method="POST">
+                            @csrf
+                            <x-jet-label for="username">Username</x-jet-label>
+                            <x-jet-input name="username" type="text" class="w-full mt-1" />
+                            <div class="flex items-center justify-center">
+                                <x-jet-button type="submit" class="mt-2 ">Submit</x-jet-button>
+                            </div>
+                            @if(session('status'))
+                            <div class="flex items-center justify-center mt-2">
+                                {{ session('status') }}
+                            </div>
+                            @endif
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/uprofile/show.blade.php
@@ -0,0 +1,63 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="w-full flex mx-4 items-center px-4 py-2">
+                    <h2 class="text-xl mr-4">{{ $profile->username }}</h2>
+                    <h3 class="text-md text-gray-800">{{ $profile->name }}</h3>
+                </div>
+                <div class="h-px mx-4 my-2 bg-gray-700"></div>
+                <div class="w-full rounded-md grid grid-cols-3 text-center">
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Total Collections
+                        </h3>
+                        {{ $profile->total_collections }}
+                    </div>
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Total Likes
+                        </h3>
+                        {{ $profile->total_likes }}
+                    </div>
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Total Photos
+                        </h3>
+                        {{ $profile->total_photos }}
+                    </div>
+                </div>
+                <div class="h-px mx-4 my-2 bg-gray-700"></div>
+                <div class="w-full rounded-md grid grid-cols-3 text-center">
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Twitter
+                        </h3>
+                        {{ $profile->twitter }}
+                    </div>
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Portfolio
+                        </h3>
+                        <a href="{{ $profile->portfolio_url }}">{{ $profile->portfolio_url }}</a>
+                    </div>
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Location
+                        </h3>
+                        {{ $profile->location }}
+                    </div>
+                </div>
+                <div class="h-px mx-4 my-2 bg-gray-700"></div>
+                <div class="w-full rounded-md text-center">
+                    <div class="my-2">
+                        <h3 class="text-lg">
+                            Bio
+                        </h3>
+                        {{ $profile->bio }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
